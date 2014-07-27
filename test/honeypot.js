7991b9eb7d367ed44202d022b748df21f4b0672f
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-27 03:13:23 -0700

    Prepare tests

---
+++ test/honeypot.js
@@ -0,0 +1,10 @@
+var assert = require('assert'),
+    Honeypot = require('../lib/Honeypot');
+
+describe('Honeypot', function () {
+  describe('#join()', function () {
+    it('should find new nodes', function (done) {
+      done();
+    });
+  });
+});
