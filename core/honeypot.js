d0b5c7dc769c3ece0e988a5dd7ff763a64f4072a
Alexander Gugel <alexander.gugel@gmail.com>
2014-08-14 22:18:52 -0700

    Implement basic honeypot

--- core/honeypot.js
+++ core/honeypot.js
@@ -4,54 +4,54 @@ var bencode = require('bencode'),
     hat = require('hat'),
     dgram = require('dgram');
 
-var BOOTSTRAP_NODES = [
+var Honeypot = function (infoHash) {
+  this.nodeIdBuffer = new Buffer(infoHash, 'hex');
+  // this.nodeIdBuffer[this.nodeIdBuffer.length-1] = this.nodeIdBuffer[this.nodeIdBuffer.length-1]++;
+  // this.nodeIdBuffer = new Buffer(this.nodeIdBuffer.toString('hex'), 'hex');
+  this.socket = dgram.createSocket('udp4');
+  var self = this;
+  this.socket.on('message', this.processMessage.bind(this));
+  this.socket.bind(6881, function() {
+    _.each(this.BOOTSTRAP_NODES, function (BOOTSTRAP_NODE) {
+      BOOTSTRAP_NODE = BOOTSTRAP_NODE.split(':');
+      this.inject(BOOTSTRAP_NODE[0], BOOTSTRAP_NODE[1]);
+    }, this);
+  }.bind(this));
+};
+
+Honeypot.prototype.BOOTSTRAP_NODES = [
   'router.bittorrent.com:6881',
   'router.utorrent.com:6881',
   'dht.transmissionbt.com:6881'
 ];
 
-var DHT = {};
-
-DHT.genNodeId = function (infoHash) {
-  // infoHash = infoHash || '04A8C73349E0FE148557C3A9BA8482E0AA67AD49';
-  // var nodeIdBuffer = new Buffer(infoHash, 'hex');
-  // nodeIdBuffer[nodeIdBuffer.length-1] = nodeIdBuffer[nodeIdBuffer.length-1]++;
-  // return nodeIdBuffer.toString('hex');
-  //
-  return new Buffer(hat(160), 'hex');
+Honeypot.prototype.inject = function (address, port, callback) {
+  this.transact({
+    y: 'q',
+    q: 'find_node',
+    a: {
+      id: this.nodeIdBuffer,
+      target: this.nodeIdBuffer
+    }
+  }, address, port, callback);
 };
 
-DHT.transact =  _.throttle(function (socket, message, address, port, callback) {
+Honeypot.prototype.transact =  _.throttle(function (transaction, address, port, callback) {
   callback = callback || _.noop;
-  var transactionID = new Buffer([~~(Math.random() * 256), ~~(Math.random() * 256)]);
-  var preparedMessage = _.defaults(message, {
-    t: transactionID
+  var transactionId = new Buffer([~~(Math.random() * 256), ~~(Math.random() * 256)]);
+  var preparedMessage = _.defaults(transaction, {
+    t: transactionId
   });
   var packet = bencode.encode(preparedMessage);
-  socket.send(packet, 0, packet.length, port, address, function(err) {
-    callback(err, transactionID.toString('hex'));
+  this.socket.send(packet, 0, packet.length, port, address, function(err) {
+    callback(err, {
+      transactionId: transactionId.toString('hex')
+    });
   });
-}, 100);
+}, 10);
 
-
-
-var socket = dgram.createSocket('udp4');
-socket.bind(6881, function() {
-  _.each(BOOTSTRAP_NODES, function (BOOTSTRAP_NODE) {
-    BOOTSTRAP_NODE = BOOTSTRAP_NODE.split(':');
-    DHT.transact(socket, {
-      y: 'q',
-      q: 'find_node',
-      a: {
-        id: new Buffer(DHT.genNodeId(), 'hex'),
-        target: new Buffer(DHT.genNodeId(), 'hex')
-      }
-    }, BOOTSTRAP_NODE[0], BOOTSTRAP_NODE[1]);
-  });
-});
-
-socket.on('message', function (transaction, rinfo) {
-  transaction = bencode.decode(transaction);
+Honeypot.prototype.processMessage = function (message, rinfo) {
+  var transaction = bencode.decode(message);
   transaction.y = transaction.y.toString();
   transaction.q = transaction.q && transaction.q.toString();
 
@@ -62,14 +62,10 @@ socket.on('message', function (transaction, rinfo) {
   if (transaction.r && transaction.r.nodes && Buffer.isBuffer(transaction.r.nodes)) {
     for (var i = 0; i < transaction.r.nodes.length; i += 26) {
       var node = compact2string(transaction.r.nodes.slice(i + 20, i + 26)).split(':');
-      DHT.transact(socket, {
-        y: 'q',
-        q: 'find_node',
-        a: {
-          id: new Buffer(DHT.genNodeId(), 'hex'),
-          target: new Buffer(DHT.genNodeId(), 'hex')
-        }
-      }, node[0], node[1]);
+      this.inject(node[0], node[1]);
     }
   }
-}.bind(this));
+};
+
+// new Honeypot('247A424720EF8AC435C31D62A33CA16BA63C56E4');
+new Honeypot(hat(160));
